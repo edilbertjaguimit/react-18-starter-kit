ed631105a21e4b64946e85841c25295af0237a89
edilbert <edilbertjagimit01@gmail.com>
2025-05-08 18:02:57 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier'],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            semi: ['error'],
+            qoutes: ['error', 'single'],
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+            prettier: [
+                'error',
+                {
+                    singleQuote: true,
+                    trailingComma: 'all',
+                    printWidth: 150,
+                    tabWidth: 4,
+                    semi: true
+                }
+            ]
+        }
+    }
+);
